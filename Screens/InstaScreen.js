bd60541363eab3bf696d3a512fca67f6549665fa
AayushiRockz <aayushisomkuwar993@gmail.com>
2021-01-01 03:15:36 +0530

    added style sheets

--- Screens/InstaScreen.js
+++ Screens/InstaScreen.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import {View,Text} from 'react-native';
+import {View,Text, StyleSheet} from 'react-native';
 
 export default class Instascreen extends React.Component{
     render(){
         return(
-            <View style={{backgroundColor:"red"}}>
+            <View style={styles.container}>
                 <Text
-                style={{color:"pink", alignSelf:"center", fontSize:100, fontWeight:"bold", marginTop:100}}>Instagram </Text>
+                style={styles.displayText}>Instagram </Text>
             </View>
         );
     }
 }
+
+
+const styles = StyleSheet.create({
+
+    container:{backgroundColor:"#fc5185",padding:40},
+    displayText:{backgroundColor:"#3fc1c9",color:"#364f6b", alignSelf:"center", fontSize:100, fontWeight:"bold", marginTop:100, borderWidth:5,borderColor:"#f5f5f5"}
+});
